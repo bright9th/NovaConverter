ae01a763c435465c0a92bffc7b928a40fd6b30ec
BB-69 <129858054+BB-69@users.noreply.github.com>
2026-05-18 22:13:44 +0700

    Clean wipe + preparation

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+﻿<script lang="ts">
+  import "./app.css";
+</script>
+
+<main id="app">
+  <h1>Nova Converter</h1>
+</main>
---
+++ src/lib/font.ts
@@ -0,0 +1,2 @@
+export const NOVA_FONT_FAMILY = "NovaModern";
+export const NOVA_FONT_URL = "/fonts/NovaModern.woff2";
